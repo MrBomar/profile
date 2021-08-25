02efc4256af0ca022219b19dffea23e2e92ca3e7
MrBomar <mrlesbomar@gmail.com>
2021-08-25 18:16:10 -0400

    Social links updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,8 @@ import './Footer.css';
 const Footer = props => {
     return (
         <footer>
-            {/* Fix Me - Need to change Acclaim to Credly */}
-            <a href ="https://www.youracclaim.com/users/leslie-bomar/badges" alt='Acclaim Profile' rel='noopener noreferrer' target='_blank'>
-                <img src='./images/Acclaim_Icon.png' height='50' width='50' alt="Acclaim Icon"/>
+            <a href ="https://www.credly.com/users/mrlesbomar" alt='Credly Profile' rel='noopener noreferrer' target='_blank'>
+                <img src='./images/Credly_Logo_Orange.png' height='50' width='auto' alt="Credly Icon"/>
             </a>
             <a href="https://github.com/MrBomar" rel='noopener noreferrer' target='_blank'>
                 <img src='/images/github-icon.png' height='50' width='50' alt="GitHub Icon"/>
@@ -20,7 +19,12 @@ const Footer = props => {
             <a href="mailto:mrlesbomar@gmail.com" rel='noopener noreferrer' target='_blank'>
                 <img src='/images/gmail-icon.png' height='50' width='50' alt='Gmail Icon'/>
             </a>
-            {/* Fix Me - Need to add Instagram */}
+            <a href="https://www.instagram.com/bomarleslie/" alt="Instagram Profile" rel='noopener noreferrer' target='_blank'>
+                <img src='/images/instagram-icon.png' height='auto' width='50' alt='Instagram Icon'/>
+            </a>
+            <a href='https://app.pluralsight.com/profile/leslie-bomar-53' alt='Pluralsight Profile' rel='noopener noreferrer' target='_blank'>
+                <img src='/images/pluralsight-icon.png' height='auto' width='50' alt='Pluralsight Icon'/>
+            </a>
             {/* Fix Me - Need to add Pluralsight */}
             <h6>This website was designed and maintained by Leslie C. Bomar III.</h6>
         </footer>
